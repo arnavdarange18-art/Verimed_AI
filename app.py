fa16892066ca8c8ddba7a3441f43f59c7ca0d67b
arnavdarange18-art <arnavdarange18@gmail.com>
2026-07-24 23:49:01 +0530

    XYZ

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request, jsonify, Response, redirect, url_for, flash, send_file
 import os
-import json
 
 from flask_login import LoginManager, login_user, logout_user, login_required, current_user
 
